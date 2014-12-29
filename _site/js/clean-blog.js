204173490d69f552d72d93a51916dae71d5a10e3
Kamal Lamichhane <lckamal@hotmail.com>
2014-12-29 10:51:20 +0545

    disqus added

--- _site/js/clean-blog.js
+++ _site/js/clean-blog.js
@@ -31,7 +31,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://lkamal.com.np/api/contact.php",
                 type: "POST",
                 data: {
                     name: name,
